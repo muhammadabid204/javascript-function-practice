70f096f941c498b77a45117f262fa15d3ebf75e8
Muhammad Abid204 <decabidrafiq2008@gmail.com>
2025-03-21 01:08:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+//console.log("hello world")
+
+//"abc".toUpperCase
+//"xyz".toUpperCase
+
+//[1,2,3].push(4);
+
+function myfunction() {
+    console.log("Welcome to Apna school")
+    console.log("We are learning js")
+}
+myfunction();
+
+function myfunction(msg ,n){//parameter -> input
+//console.log(msg *n);
+//myfunction("i love in js,100");//argument
+}
+
+//function -> 2 number sum
+
+function sum(x,y){
+  console.log(x+y)  
+}
